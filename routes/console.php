a451378af5675c72a4b4595df85b68d7a47e2d93
marcosxns68 <marcos.xns@gmail.com>
2026-09-08 23:14:11 -0300

    feat: adicionar base do Sutoorii Tickets

---
+++ routes/console.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('tickets:mark-overdue')->everyFiveMinutes()->withoutOverlapping();
+Schedule::command('tickets:notify-deadlines')->dailyAt('09:00')->withoutOverlapping();
+Schedule::command('tickets:generate-recurring')->everyMinute()->withoutOverlapping();
+Schedule::command('attachments:expire')->dailyAt('02:00')->withoutOverlapping();
