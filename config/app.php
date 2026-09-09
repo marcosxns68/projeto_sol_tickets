415c5490d2b05cc3a082b2d65aa4932b35435cc0
marcosxns68 <marcos.xns@gmail.com>
2026-09-08 23:13:45 -0300

    feat: adicionar base do Sutoorii Tickets

---
+++ config/app.php
@@ -0,0 +1,2 @@
+<?php
+return ['name'=>env('APP_NAME','Sutoorii Tickets'),'env'=>env('APP_ENV','production'),'debug'=>(bool)env('APP_DEBUG',false),'url'=>env('APP_URL'),'timezone'=>env('APP_TIMEZONE','America/Sao_Paulo'),'locale'=>'pt_BR','fallback_locale'=>'pt_BR','faker_locale'=>'pt_BR','cipher'=>'AES-256-CBC','key'=>env('APP_KEY'),'previous_keys'=>array_filter(explode(',',env('APP_PREVIOUS_KEYS',''))),'maintenance'=>['driver'=>'file']];
