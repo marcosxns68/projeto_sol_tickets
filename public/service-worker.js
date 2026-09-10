565685256412e2decccecbe295a96bb5c05f6a0a
marcosxns68 <marcos.xns@gmail.com>
2026-09-10 20:32:46 -0300

    fix: invalidar cache antigo de estilos

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
-const CACHE='sutoorii-tickets-v1';const ASSETS=['/css/app.css','/manifest.webmanifest','/icons/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))) });
+const CACHE='sutoorii-tickets-v2';
+const ASSETS=['/manifest.webmanifest','/icons/icon.svg'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+    self.clients.claim(),
+  ]));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin) return;
+
+  event.respondWith(
+    fetch(event.request).catch(()=>caches.match(event.request))
+  );
+});
