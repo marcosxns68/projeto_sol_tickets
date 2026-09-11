32b064dbbef49a8bba3bf00a78d28e6e86015fa5
marcosxns68 <marcos.xns@gmail.com>
2026-09-11 16:22:50 -0300

    fix: remover ativação bloqueante do deploy das requisições

--- public/index.php
+++ public/index.php
@@ -1,16 +1,8 @@
 <?php
 
-use App\Support\DeploymentMigration;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-require_once __DIR__.'/deployment-bootstrap.php';
-
-DeploymentBootstrap::apply(
-    __DIR__.'/../storage/app/private/deploy-release.zip',
-    dirname(__DIR__)
-);
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
@@ -19,11 +11,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-DeploymentMigration::run(
-    $app,
-    __DIR__.'/../storage/framework/deploy-migrate',
-    [__DIR__.'/release.zip', __DIR__.'/deploy.php']
-);
 
 $app->handleRequest(Request::capture());
