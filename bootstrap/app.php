835f52c359e9ebb9f321d3b80d556bcad39ff050
marcosxns68 <marcos.xns@gmail.com>
2026-09-11 15:24:37 -0300

    feat: register integration middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AuthenticateIntegration;
 use App\Http\Middleware\RequirePermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'permission' => RequirePermission::class,
+            'integration' => AuthenticateIntegration::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {})
