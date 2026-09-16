72573370a70428d9c23cfb6dcdf90808e4d2655e
marcosxns68 <marcos.xns@gmail.com>
2026-09-16 16:56:29 -0300

    test: alinhar regressão ao CSS global do layout

--- tests/Feature/TicketCreateVisibilityRegressionTest.php
+++ tests/Feature/TicketCreateVisibilityRegressionTest.php
@@ -8,7 +8,7 @@ class TicketCreateVisibilityRegressionTest extends TestCase
 {
     public function test_progressive_create_sections_force_hidden_elements_out_of_layout(): void
     {
-        $source = file_get_contents(resource_path('views/tickets/create.blade.php'));
+        $source = file_get_contents(resource_path('views/layouts/app.blade.php'));
 
         $this->assertStringContainsString(
             '.ticket-create-v2 [hidden]{display:none!important}',
